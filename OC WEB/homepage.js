0684ca436c8ad1ea71f3bf8e796e6d2e2f34e40b
Keith <11corteschristopher@gmail.com>
2025-05-29 23:27:35 +0800

    Add files via upload

---
+++ OC WEB/homepage.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("CompSci").addEventListener("click", function () {
+    window.location.href = "OtherPages/CompSci.html";
+  });
+
+  document.getElementById("Crim").addEventListener("click", function () {
+    window.location.href = "OtherPages/Crim.html";
+  });
+
+  document.getElementById("HM").addEventListener("click", function () {
+    window.location.href = "OtherPages/HM.html";
+  });
+
+  document.getElementById("BAMF").addEventListener("click", function () {
+    window.location.href = "OtherPages/BAMF.html";
+  });
+
+  document.getElementById("MA").addEventListener("click", function () {
+    window.location.href = "OtherPages/MA.html";
+  });
+
+  document.getElementById("SE").addEventListener("click", function () {
+    window.location.href = "OtherPages/SE.html";
+  });
+
+  document.getElementById("EE").addEventListener("click", function () {
+    window.location.href = "OtherPages/EE.html";
+  });
+});
